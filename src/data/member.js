bd710200dabd417a98b90be65a9100204410e33d
Thadzy <Thadzy025@gmail.com>
2024-05-15 14:13:02 +0700

    Update member list Video, Link, Interest

--- src/data/member.js
+++ src/data/member.js
@@ -5,9 +5,12 @@ export const member_list = [
     name: "สุชัจจ์ ทรัพย์เสถียร",
     nickname: "ปลื้ม",
     occupation: "คณะวิศวกรรมศาสตร์ สาขาวิศวกรรมคอมพิวเตอร์",
+    interest: "Data Science",
     phone: 0922568868,
     email: "suchut@outlook.com",
     ig: "pluemssn",
+    video: "Robo Advisor"
+    link: "https://youtu.be/GkVIZHaiSzc",
   },
   {
     image_path: "member/Super AI SS 400086.jpg",
@@ -15,9 +18,12 @@ export const member_list = [
     name: "สิทธการย์ อุดมวงศ์วัฒนา",
     nickname: "เต้",
     occupation: "คณะปัญญาประดิษฐ์และอินเทอร์เน็ตของสรรพสิ่ง",
+    interest: "Computer Vision, AI for financial",
     phone: 0940103690,
     email: "Taetaesitthakarn@gmail.com",
     ig: "Ttaestk",
+    video: "An Introduction to Mask R-CNN in Theory"
+    link: "https://youtu.be/9XdKnOC4jUc?si=4Jy84pzuGH03rMD6",
   },
   {
     image_path: "member/Super AI SS 400096.jpg",
@@ -25,9 +31,12 @@ export const member_list = [
     name: "ศุภวิชญ์ กิจการเจริญสิน",
     nickname: "มาร์ค",
     occupation: "คณะพาณิชยศาสตร์และการบัญชี สาขาสถิติ",
+    interest: "Data Science",
     phone: 0837872744,
     email: "Suphawitchmark@hotmail.com",
     ig: "m_suphawitch",
+    video: "Data scientist ใน bank ฉบับเด็กฝึกงาน"
+    link: "https://youtu.be/h-8sWOqyXOI?si=LojjCxaYXiS6Lxa8",
   },
   {
     image_path: "member/Super AI SS 400139.jpg",
@@ -35,9 +44,12 @@ export const member_list = [
     name: "คเณศ แสงมิ่ง",
     nickname: "ฮ่องเต้",
     occupation: "คณะพาณิชยศาสตร์และการบัญชี สาขาบัญชี",
+    interest: "LLM",
     phone: 0970705042,
     email: "kanet2546@gmail.com",
     ig: "kanhnap",
+    video: "SnakeGame ด้วย Computer vision"
+    link: "https://youtu.be/4buWhH1sXyI?si=ZvYzxN1jMUUs2j4N",
   },
   {
     image_path: "member/Super AI SS 400140.jpg",
@@ -45,9 +57,12 @@ export const member_list = [
     name: "ณัฐธิดา บารมี",
     nickname: "จันทร์เจ้า",
     occupation: "คณะวิศวกรรมศาสตร์ สาขาวิศวกรรมไฟฟ้า",
+    interest: "AIoT , Big data",
     phone: 0634169737,
     email: "moonny1621@gmail.com",
     ig: "jw.junjaow",
+    video: "เปิดโลก AI จิ๋ววว"
+    link: "https://www.youtube.com/watch?v=L3mDIOgD9JQ&t=",
   },
   {
     image_path: "member/Super AI SS 400187.jpg",
@@ -55,9 +70,12 @@ export const member_list = [
     name: "ปวิธ ปัญญาสิริกุล",
     nickname: "กันต์",
     occupation: "คณะวิศวกรรมศาสตร์ สาขาวิศวกรรมไฟฟ้า",
+    interest: "Data Science, Big data",
     phone: 0805247441,
     email: "pawithgun41@gmail.com",
     ig: "pawithgun",
+    video: "Confusion Matrix"
+    link: "https://www.youtube.com/watch?v=pFSlCdrXyyA",
   },
   {
     image_path: "member/Super AI SS 400286.jpg",
@@ -65,9 +83,12 @@ export const member_list = [
     name: "อุกฤษฏ์ ดำแดง",
     nickname: "โอ",
     occupation: "วิศวกรซอฟท์แวร์ @ ทิพยประกันภัย",
+    interest: "MLLM",
     phone: 0944981482,
     email: "ougridd@gmail.com",
     ig: "-",
+    video: "Reducing LLM Hallucination with RAG"
+    link: "https://youtu.be/8uHYD3HiFm8?si=PjAnRONPTrefnRZW",
   },
   {
     image_path: "member/Super AI SS 400300.jpg",
@@ -75,9 +96,12 @@ export const member_list = [
     name: "ปัญจพงศ์ ภูบาลชื่น",
     nickname: "ข้าวปั้น",
     occupation: "คณะวิศวกรรมศาสตร์ สาขาวิศวกรรมคอมพิวเตอร์ (หลักสูตรนานาชาติ)",
+    interest: "LLM, Neurosci, QuantumML",
     phone: 0972163593,
     email: "panjapongpoolbanchean@gmail.com",
     ig: "khawpun_kub",
+    video: "Speech Dysarthria Detection"
+    link: "https://youtu.be/6FYHRexOqlo?si=S7PC26MzRVcjUGGD",
   },
   {
     image_path: "member/Super AI SS 400627.jpg",
@@ -85,9 +109,25 @@ export const member_list = [
     name: "ภาณุพงษ์ สภานนท์",
     nickname: "โหน่ง",
     occupation: "คณะวิศวกรรมศาสตร์ สาขาวิศวกรรมคอมพิวเตอร์และเทคโนโลยี",
+    interest: "LLM, Signal, NLP",
     phone: 0972733779,
     email: "nongft09090@gmail.com",
     ig: "noboru_9925",
+    video: "Web App AI โฮสบน Goolge Colab โดยใช้ไลบรารี่ Anvil"
+    link: "https://www.youtube.com/watch?v=oYt6HoIAJ78",
+  },
+  {
+    image_path: "member/Super AI SS 400627.jpg",
+    id: 400627,
+    name: "ธีธัช SERVICES",
+    nickname: "ธี",
+    occupation: "คณะวิศวกรรมศาสตร์ สาขาวิศวกรรมคอมพิวเตอร์และเทคโนโลยี",
+    interest: "LLM",
+    phone: 0972733779,
+    email: "ntheethut@gmail.com",
+    ig: "T",
+    video: "Text Summarization"
+    link: "https://www.youtube.com/watch?v=B7neNHOFLyI&t=389s",
   },
   {
     image_path: "member/Super AI SS 400688.jpg",
@@ -95,9 +135,12 @@ export const member_list = [
     name: "โสภณวิชญ์ เจริญวงศ์",
     nickname: "เมจิค",
     occupation: "คณะปัญญาประดิษฐ์และอินเทอร์เน็ตของสรรพสิ่ง",
+    interest: "Image Classification",
     phone: 0898168454,
     email: "magiczakung@gmail.com",
     ig: "magic_spw",
+    video: "Adversarial attack"
+    link: "https://youtu.be/zAEJUIEmP24?si=vDYSd9NiwCpxYEe1",
   },
   {
     image_path: "member/Super AI SS 400823.jpg",
@@ -105,9 +148,12 @@ export const member_list = [
     name: "สิปปภาส ปิติวรรณ",
     nickname: "ปลื้ม",
     occupation: "คณะวิศวกรรมศาสตร์ สาขาวิศวกรรมหุ่นยนต์และปัญญาประดิษฐ์",
+    interest: "LLM, Data Science , Data Engineer, NLP",
     phone: 0999879195,
     email: "s.pitiwan.w@gmail.com",
     ig: "_pluem.pl",
+    video: "Transformer 101"
+    link: "https://youtu.be/-UB5R0JdhWY?si=Nxn2B7CTTyFn_9vI",
   },
   {
     image_path: "member/Super AI SS 400943.jpg",
@@ -115,9 +161,12 @@ export const member_list = [
     name: "ทัดชัย  สุขสราญ",
     nickname: "ทัชชี่",
     occupation: "คณะวิศวกรรมศาสตร์ สาขาวิศวกรรมหุ่นยนต์และระบบอัตโนมัติ",
+    interest: "Quantum AI",
     phone: 0844524645,
     email: "Thadzy025@gmail.com",
     ig: "sh.adzyt_",
+    video: "การนำ AI ไปใช้จริงผ่าน Web application React + FastAPI"
+    link: "https://youtu.be/6829srkigsA?si=FI9WKVrreF73Ymvt",
   },
   {
     image_path: "member/Super AI SS 400968.jpg",
@@ -125,9 +174,11 @@ export const member_list = [
     name: "กนกกฤษฏิ์ กังวาลเกียรติ",
     nickname: "กิต",
     occupation: "คณะวิศวกรรมคอมพิวเตอร์ สาขาวิทยาศาสตร์ข้อมูลสุขภาพ",
-    phone: 0935820530,
+    interest: "AI in Healthcare, NLP, Image",
     email: "Kanokkrit131@gmail.com",
     ig: "Kanokkrit_",
+    video: "Optimization เบื้องหลังความสำเร็จของ AI"
+    link: "https://youtu.be/TUOLOsfS8YE?si=AcF9zJduvRW-hw2D",
   },
   {
     image_path: "member/Super AI SS 400969.jpg",
@@ -135,9 +186,12 @@ export const member_list = [
     name: "ปิยะธิดา ทองภู",
     nickname: "พลอย",
     occupation: "คณะวิศวกรรมคอมพิวเตอร์ สาขาวิทยาศาสตร์ข้อมูลสุขภาพ",
+    interest: "AI in Healthcare",
     phone: 0933382846,
     email: "Piyatidaploy2001@gmail.com",
     ig: "ploy_platinum",
+    video: "การวัดประสิทธิภาพโมเดลแบบ Classification"
+    link: "https://youtu.be/ZvRE4aGUWx0?si=bPxewYUFbvOjF7l3",
   },
   {
     image_path: "member/Super AI SS 400991.jpg",
@@ -145,9 +199,12 @@ export const member_list = [
     name: "วสุพล ชินสาโรจน์",
     nickname: "ทิว",
     occupation: "คณะวิศวกรรมศาสตร์ สาขาวิศวกรรมหุ่นยนต์และปัญญาประดิษฐ์",
+    interest: "NLP, Computer Vision",
     phone: 0968262069,
     email: "tewwasupol@gmail.com",
     ig: "tewwasupol",
+    video: "Linear Regression from Scratch"
+    link: "https://youtu.be/nO5knFanUrA?si=BykeFhDWw8pwzgtt",
   },
   {
     image_path: "member/Super AI SS 401024.jpg",
@@ -155,9 +212,12 @@ export const member_list = [
     name: "ภคิน จตุปัญญาโชติกุล",
     nickname: "ภูผา",
     occupation: "คณะเทคโนโลยีและสารสนเทศ สาขาวิชาวิทยาการข้อมูลและการวิเคราะห์เชิงธุรกิจ",
+    interest: "AI for Business",
     phone: 0969653256,
     email: "phupah31@gmail.com",
     ig: "pooppha",
+    video: ""
+    link: "https://youtu.be/SSUyOnJkFbY?si=cuWivEH-gLFhiXHD",
   },
   {
     image_path: "member/Super AI SS 401089.jpg",
@@ -165,9 +225,12 @@ export const member_list = [
     name: "ธีรกานต์ ทวีรัตน์",
     nickname: "ภีม",
     occupation: "วิศวกรรมศาสตร์ สาขาวิศวกรรมวัดคุมและเครื่องมือวัด",
+    interest: "AI for Business",
     phone: 0869430800,
     email: "andant.yt@gmail.com",
     ig: "su.peem",
+    video: "วิธีเรียนรู้ AI จากไม่มีพื้นฐาน ตามคนอื่นทันแน่นอน!!"
+    link: "https://youtu.be/SSUyOnJkFbY?si=cuWivEH-gLFhiXHD",
   },
   {
     image_path: "member/Super AI SS 401119.jpg",
@@ -175,9 +238,12 @@ export const member_list = [
     name: "ปัทมา ยอดทอง",
     nickname: "ฝ้าย",
     occupation: "คณะวิทยาการคอมพิวเตอร์",
+    interest: "Data Science, NLP",
     phone: 0968961859,
     email: "patthama.fai1144@gmail.com",
     ig: "fai.ptm",
+    video: "ปัญหาที่เกิดจาก Bias Data"
+    link: "https://youtu.be/Ho0PQFOslvQ?si=Ylq2JMe1GIS5Xfvd",
   },
   {
     image_path: "member/Super AI SS 401313.jpg",
@@ -185,9 +251,12 @@ export const member_list = [
     name: "ฐิรพรรษ ปานทอง",
     nickname: "เต้",
     occupation: "คณะวิทยาการคอมพิวเตอร์",
+    interest: "Big Data, DL",
     phone: 0641672257,
     email: "0935029091pote@gmail.com",
     ig: "farlos.ta3",
+    video: "Neural networks ฉบับ Easy understanding"
+    link: "https://youtu.be/Zd10YYX3MrE?si=KjWOau2_MukGLO4Q",
   },
   {
     image_path: "member/Super AI SS 401330.jpg",
@@ -195,9 +264,12 @@ export const member_list = [
     name: "สรวิชญ์ ใหม่ชุ่ม",
     nickname: "บุ๊คบิ๊ก",
     occupation: "Software developer/ เรียน Physics ",
+    interest: "AI for Investment",
     phone: 0811562940,
     email: "msorawich@gmail.com",
     ig: "-",
+    video: "สร้าง Machine learning อย่างง่าย ได้ด้วย Bayesian classification"
+    link: "https://youtu.be/v8nUNjmQH0A?si=nsvQMuK3-01OJPOw",
   },
   {
     image_path: "member/Super AI SS 401983.jpg",
@@ -205,9 +277,12 @@ export const member_list = [
     name: "ศิริศักดิ์ เอาทารย์สกุล",
     nickname: "โฟล์ค",
     occupation: "คณะวิศวกรรมศาสตร์ สาขาวิศวกรรมไฟฟ้าอิเล็กทรอนิกส์และโทรคมนาคม",
+    interest: "LLM, AIoT",
     phone: 0819955180,
     email: "aotarnsakul.sirisak@gmail.com",
     ig: "-",
+    video: "จากสตาร์คเทคสู่โลกแห่งความจริง"
+    link: "https://youtu.be/2jP7u7J0u8o?si=PNDkYALnbzaSsEXb",
   },
   {
     image_path: "member/Super AI SS 402091.jpg",
@@ -215,9 +290,12 @@ export const member_list = [
     name: "ณัฐกฤต มาชู",
     nickname: "ดัง",
     occupation: "คณะวิศวกรรมศาสตร์ สาขาวิศวกรรมคอมพิวเตอร์",
+    interest: "DL",
     phone: 0995681977,
     email: "dungmachoo@gmail.com",
     ig: "Dung Machoo",
+    video: "Hello World of Machine Learning"
+    link: "https://www.youtube.com/watch?v=vp6IVF9qV9U",
   },
   {
     image_path: "member/Super AI SS 402978.jpg",
@@ -225,9 +303,12 @@ export const member_list = [
     name: "ตรรกพล ตันธวัช",
     nickname: "ดิว",
     occupation: "KOSEN KMITL แมคคาทรอนิกส์",
+    interest: "Transformer",
     phone: 0918812743,
     email: "takapoltanthavach@gmail.com",
     ig: "dew24888",
+    video: "Basic Neural Networks ใน 10 นาที"
+    link: "https://www.youtube.com/watch?v=qBxn9v_JuFw",
   },
   {
     image_path: "member/Super AI SS 404136.jpg",
@@ -235,9 +316,12 @@ export const member_list = [
     name: "ธนบริบูรณ์ ช่อเกตุ  อภิพัฒน์สกุล",
     nickname: "เชน",
     occupation: "คณะเทคโนโลยีระบบเกษตร",
+    interest: "Ai for Agriculture",
     phone: 0961494747,
     email: "thanaboriboon.c@gmail.com",
     ig: "chxxn_nxxhc",
+    video: "การประยุกต์ใช้เทคโนโลยี AI กับภาคการเกษตร"
+    link: "https://youtu.be/7vwQAxd2IP0?si=rLybOxruUPDdfsgh",
   },
   {
     image_path: "member/Super AI SS 404594.jpg",
@@ -245,9 +329,12 @@ export const member_list = [
     name: "เชษฐ์ ทัดทอง",
     nickname: "ต้น",
     occupation: "ขายของ online",
+    interest: "Image, AIoT",
     phone: 0826819889,
     email: "chet.tudtong@gmail.com",
     ig: "-",
+    video: "AI-Neuron-concept"
+    link: "https://youtu.be/HbUofG2snAM",
   },
   {
     image_path: "member/Super AI SS 406367.jpg",
@@ -255,9 +342,12 @@ export const member_list = [
     name: "ณัฐกิตติ์ สุขแจ่ม",
     nickname: "ดิว",
     occupation: "คณะวิศวกรรมโยธา สาขาทรัพยากรน้ำ",
+    interest: "Data Science,Image",
     phone: 0855542000,
     email: "natthakit.workspace@gmail.com",
     ig: "natthakit_eiei",
+    video: "Logistic Regression With No Code"
+    link: "https://youtu.be/MFnI7IvanA0?si=YphyKKAtN2Fb60KY",
   },
   {
     image_path: "member/Super AI SS 406395.jpg",
@@ -265,8 +355,11 @@ export const member_list = [
     name: "กฤตภาส หาญช้าง",
     nickname: "ป๋อ",
     occupation: "วิศวกรรมอุตสาหการ",
+    interest: "Data Science,Big data, NLP(LLM)",
     phone: 0892885120,
     email: "krittapadpor12348@gmail.com",
     ig: "por_kph",
+    video: "A Brief history of AI"
+    link: "https://www.youtube.com/watch?v=zh1jln2_C4E&t=1s",
   },
 ];
