44b85724214e89824127a2233051d156068e4a26
Thadzy <Thadzy025@gmail.com>
2024-05-15 22:27:36 +0700

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import "./App.css";
 import Home from "./pages/Home";
 import Member from "./pages/Member";
 import Staff from "./pages/Staff";
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import NavScrollExample from "./components/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [page, setPage] = useState();
@@ -22,20 +30,23 @@ function App() {
   });
 
   return (
-    <div className="body">
-      <ThemeProvider theme={theme}>
-        <HashRouter scrollIndicatorInsets={{ right: 1 }}>
-          <Routes>
-            <Route exact path="/" element={<Home setPage={setPage} />} />
-            <Route
-              exact
-              path="/member"
-              element={<Member setPage={setPage} />}
-            />
-            <Route exact path="/staff" element={<Staff setPage={setPage} />} />
-          </Routes>
-        </HashRouter>
-      </ThemeProvider>
+    <div className="Navbar">
+      <NavScrollExample />
+        <div className="body">
+          <ThemeProvider theme={theme}>
+            <HashRouter scrollIndicatorInsets={{ right: 1 }}>
+              <Routes>
+                <Route exact path="/" element={<Home setPage={setPage} />} />
+                <Route
+                  exact
+                  path="/member"
+                  element={<Member setPage={setPage} />}
+                />
+                <Route exact path="/staff" element={<Staff setPage={setPage} />} />
+              </Routes>
+            </HashRouter>
+          </ThemeProvider>
+        </div>
     </div>
   );
 }
